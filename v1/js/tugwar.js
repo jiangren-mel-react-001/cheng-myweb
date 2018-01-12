9786cbe2c27d6a5f7123b76715db3507a0e253b7
tonychangcheng <cheng.chang2@pactera.com>
2018-01-12 15:46:11 +1100

    M3 V7

--- v1/js/tugwar.js
+++ v1/js/tugwar.js
@@ -1,21 +1,39 @@
 
-function move(robot, nextCell) {
-    if (nextCell.classList.contains('cell')) {
-        robot.classList.remove('current');
-        robot.innerHTML = '';
-        nextCell.classList.add('current');
-        nextCell.innerHTML = 'R';
+var state = {
+    cells: [],
+    currentPosition: 0,
+    init: function() {
+        this.cells = document.getElementsByClassName('cell');
+        for (var i = 0; i < this.cells.length; i++) {
+            if (this.cells[i].classList.contains('current')) {
+                this.currentPosition = i;
+                break;
+            }
+        }
+    },
+    move(step) {
+        var nextPosition = this.currentPosition + step;
+        if (this.positionAvailable(nextPosition)) {
+            var robot = this.cells[this.currentPosition];
+            var nextCell = this.cells[nextPosition];
+            robot.classList.remove('current');
+            robot.innerHTML = '';
+            nextCell.classList.add('current');
+            nextCell.innerHTML = 'R';
+            this.currentPosition = nextPosition;
+        }
+    },
+    positionAvailable(nextPosition) {
+        return nextPosition >= 0 && nextPosition < this.cells.length;
     }
 }
 
+state.init();
+
 function onClickLeft() {
-    var robot = (document.getElementsByClassName('current'))[0];
-    var nextCell = robot.previousElementSibling;
-    move(robot, nextCell);
+    state.move(-1);
 }
 
 function onClickRight() {
-    var robot = (document.getElementsByClassName('current'))[0];
-    var nextCell = robot.nextElementSibling;
-    move(robot, nextCell);
+    state.move(1);
 }
