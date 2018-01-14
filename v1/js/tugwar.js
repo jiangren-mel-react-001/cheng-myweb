fb0b6fb3f829568a0538505bfd352d6a7a169540
tonychangcheng <cheng.chang2@pactera.com>
2018-01-14 20:38:14 +1100

    class

--- v1/js/tugwar.js
+++ v1/js/tugwar.js
@@ -1,16 +1,11 @@
 
-var state = {
-    cells: [],
-    currentPosition: 0,
-    init: function() {
+class Game {
+    constructor() {
+        // get data from Ajax
         this.cells = document.getElementsByClassName('cell');
-        for (var i = 0; i < this.cells.length; i++) {
-            if (this.cells[i].classList.contains('current')) {
-                this.currentPosition = i;
-                break;
-            }
-        }
-    },
+        this.currentPosition = 2;
+        this.length = 5
+    };
     move(step) {
         var nextPosition = this.currentPosition + step;
         if (this.positionAvailable(nextPosition)) {
@@ -22,18 +17,10 @@ var state = {
             nextCell.innerHTML = 'R';
             this.currentPosition = nextPosition;
         }
-    },
+    };
     positionAvailable(nextPosition) {
         return nextPosition >= 0 && nextPosition < this.cells.length;
     }
 }
 
-state.init();
-
-function onClickLeft() {
-    state.move(-1);
-}
-
-function onClickRight() {
-    state.move(1);
-}
+let aGame = new Game();
