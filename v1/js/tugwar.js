530335054a0dcb7271f8a34ab323c156d9f8d64a
tonychangcheng <cheng.chang2@pactera.com>
2018-01-14 19:31:59 +1100

    create the problem

--- v1/js/tugwar.js
+++ v1/js/tugwar.js
@@ -1,21 +1,31 @@
 
-function move(robot, nextCell) {
-    if (nextCell.classList.contains('cell')) {
-        robot.classList.remove('current');
-        robot.innerHTML = '';
-        nextCell.classList.add('current');
-        nextCell.innerHTML = 'R';
+var game = {
+    robotIcon: 'R',
+    currentCell: null,
+    init: function() {
+        var cells = document.querySelectorAll('.cell');
+        cells.forEach(function(aCell) {
+            if (aCell.textContent === this.robotIcon) {
+                this.currentCell = aCell;
+            }
+        });
+    },
+    move: function(robot, nextCell) {
+        if (nextCell.classList.contains('cell')) {
+            robot.textContent = '';
+            nextCell.textContent = this.robotIcon;
+        }
+    },
+    onClickLeft: function() {
+        var nextCell = this.currentCell.previousElementSibling;
+        this.move(this.currentCell, nextCell);
+        this.currentCell = nextCell;
+    },
+    onClickRight: function() {
+        var nextCell = this.currentCell.nextElementSibling;
+        this.move(this.currentCell, nextCell);
+        this.currentCell = nextCell;
     }
 }
 
-function onClickLeft() {
-    var robot = (document.getElementsByClassName('current'))[0];
-    var nextCell = robot.previousElementSibling;
-    move(robot, nextCell);
-}
-
-function onClickRight() {
-    var robot = (document.getElementsByClassName('current'))[0];
-    var nextCell = robot.nextElementSibling;
-    move(robot, nextCell);
-}
+game.init();
