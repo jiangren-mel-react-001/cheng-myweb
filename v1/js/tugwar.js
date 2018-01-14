fa1830456fac8f1f57d879f73d734329ea2a7538
tonychangcheng <cheng.chang2@pactera.com>
2018-01-14 21:55:17 +1100

    cdn react

--- v1/js/tugwar.js
+++ v1/js/tugwar.js
@@ -1,4 +1,5 @@
 
+
 class Game {
     constructor() {
         // get data from Ajax
@@ -16,20 +17,21 @@ class Game {
         return nextPosition >= 0 && nextPosition < this.length;
     };
     render() {
-        let map = document.createElement('div');
-        map.setAttribute('id', 'game-map');
-        map.classList.add('col-sm-8', 'col-lg-4', 'd-flex', 'text-center')
+        let cells = [];
         for (let i = 0; i < this.length; i++) {
-            let cell = document.createElement('div');
-            cell.classList.add('cell', 'border', 'border-secondary', 'w-25');
-            if (i === this.currentPosition) {
-                cell.textContent = 'R';
-            }
-            map.appendChild(cell);
+            let aCell = React.createElement('div', {
+                className: 'cell border border-secondary w-25'
+            }, i === this.currentPosition ? 'R' : '');
+            cells.push(aCell);
         }
-        let app = document.getElementById('game');
-        let gameMap = document.getElementById('game-map');
-        app.replaceChild(map, gameMap);
+        let map = React.createElement('div', {
+            id: 'game-map',
+            className: 'col-sm-8 col-lg-4 d-flex text-center'
+        }, cells);
+        ReactDOM.render(
+            map,
+            document.getElementById('game-map')
+        );
     }
 }
 
