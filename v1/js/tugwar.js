2410f850240f822cacf16ee435e36dfc048688dd
tonychangcheng <cheng.chang2@pactera.com>
2018-01-14 21:19:33 +1100

    M9 V1

--- v1/js/tugwar.js
+++ v1/js/tugwar.js
@@ -2,25 +2,36 @@
 class Game {
     constructor() {
         // get data from Ajax
-        this.cells = document.getElementsByClassName('cell');
-        this.currentPosition = 2;
-        this.length = 5
+        this.currentPosition = 5;
+        this.length = 10;
     };
     move(step) {
         var nextPosition = this.currentPosition + step;
         if (this.positionAvailable(nextPosition)) {
-            var robot = this.cells[this.currentPosition];
-            var nextCell = this.cells[nextPosition];
-            robot.classList.remove('current');
-            robot.innerHTML = '';
-            nextCell.classList.add('current');
-            nextCell.innerHTML = 'R';
             this.currentPosition = nextPosition;
+            this.render();
         }
     };
     positionAvailable(nextPosition) {
-        return nextPosition >= 0 && nextPosition < this.cells.length;
+        return nextPosition >= 0 && nextPosition < this.length;
+    };
+    render() {
+        let map = document.createElement('div');
+        map.setAttribute('id', 'game-map');
+        map.classList.add('col-sm-8', 'col-lg-4', 'd-flex', 'text-center')
+        for (let i = 0; i < this.length; i++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell', 'border', 'border-secondary', 'w-25');
+            if (i === this.currentPosition) {
+                cell.textContent = 'R';
+            }
+            map.appendChild(cell);
+        }
+        let app = document.getElementById('game');
+        let gameMap = document.getElementById('game-map');
+        app.replaceChild(map, gameMap);
     }
 }
 
 let aGame = new Game();
+aGame.render();
